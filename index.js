10773eb6f3e1837f54d993b248367bf5be009472
Zack Stevens <zack.st7@gmail.com>
2021-12-19 08:17:00 -0700

    Initial commit, added base action logic and config

---
+++ index.js
@@ -0,0 +1,41 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const {Octokit} = require("@octokit/rest");
+
+try {
+    const token = core.getInput('token')
+    const setIncludeAdmins = core.getBooleanInput('set-include-admins')
+    const includeAdmins = core.getBooleanInput('include-admins')
+    const owner = core.getInput('owner')
+    const repo = core.getInput('repo')
+    const branch = core.getInput('branch')
+    const octokit = new Octokit({
+        auth: token,
+    });
+    if (setIncludeAdmins) {
+        const params = {
+            owner,
+            repo,
+            branch
+        }
+        if (includeAdmins) {
+            octokit.rest.repos.setAdminBranchProtection(params)
+                .then(() => {
+                    core.info("Enabled include admins setting")
+                }).catch(error => {
+                    core.error(`Error encountered attempting to enable include admins setting`)
+                    core.setFailed(error)
+            });
+        } else {
+            octokit.rest.repos.deleteAdminBranchProtection(params)
+                .then(() => {
+                    core.info("Disabled include admins setting")
+                }).catch(error => {
+                core.error(`Error encountered attempting to disable include admins setting`)
+                core.setFailed(error)
+            });
+        }
+    }
+} catch (error) {
+    core.setFailed(error)
+}
